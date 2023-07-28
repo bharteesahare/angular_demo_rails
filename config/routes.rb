1ee3330b27065058fc52415a1f3b55c8c7d15443
bhartee-sahare <bhartee@vebuin.com>
2023-07-28 15:11:34 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'static_pages/index'
+  resources :restaurants
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "static_pages#index"
+end
